25704a2b02ea61987551023709590f14aedfd9f3
Albert Safin <xzfcpw@gmail.com>
2020-04-21 07:55:25 +0000

    tests: Test --keep

--- tests/t04-env-pure-impure.sh
+++ tests/t04-env-pure-impure.sh
@@ -1,20 +1,50 @@
 #!/bin/sh
 . ./lib.sh
 
-export SOME_VAR=some-var-value
-
-run_inline << 'EOF'
-#!/usr/bin/env cached-nix-shell
-#! nix-shell -i sh -p
-
-echo ${SOME_VAR-doesnt-have-some-var}
+put tmp/prefix.nix << 'EOF'
+with import <nixpkgs> { };
+let
+  add-prefix = stdenv.mkDerivation {
+    name = "cached-nix-shell-test-add-prefix";
+    unpackPhase = ":";
+    installPhase = ":";
+    setupHook = writeText "setup-hook" ''
+      export FOO=prefix:$FOO
+    '';
+  };
+in mkShell { buildInputs = [ add-prefix ]; }
 EOF
-check_contains "some-var-value"
 
-run_inline << 'EOF'
-#!/usr/bin/env cached-nix-shell
-#! nix-shell -i sh -p --pure
+export FOO=foo-value
+unset BAR
 
-echo ${SOME_VAR-doesnt-have-some-var}
-EOF
-check_contains "doesnt-have-some-var"
+
+run cached-nix-shell -p --run 'echo ${FOO-doesnt-have-foo}'
+check_contains "^foo-value$"
+
+run cached-nix-shell -p --pure --run 'echo ${FOO-doesnt-have-foo}'
+check_contains "^doesnt-have-foo$"
+check_fast
+
+run cached-nix-shell -p --pure --keep FOO --run 'echo ${FOO-doesnt-have-foo}'
+check_contains "^foo-value$"
+# TODO: this should not invalidate the cache
+skip check_fast
+
+
+run cached-nix-shell ./tmp/prefix.nix --keep FOO \
+	--run 'echo ${FOO-doesnt-have-foo}'
+check_contains "^prefix:foo-value$"
+
+run cached-nix-shell ./tmp/prefix.nix --pure --keep FOO \
+	--run 'echo ${FOO-doesnt-have-foo}'
+check_contains "^prefix:foo-value$"
+check_fast
+
+export BAR=bar-value
+run cached-nix-shell ./tmp/prefix.nix --pure --keep FOO --keep BAR \
+	--run 'echo ${FOO-doesnt-have-foo}; echo ${BAR-doesnt-have-bar}'
+check_contains "^prefix:foo-value$"
+check_contains "^bar-value$"
+# TODO: this should not invalidate the cache
+skip check_fast
