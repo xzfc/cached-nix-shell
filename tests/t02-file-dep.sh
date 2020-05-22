e516c7d97d960dcebc70084efec66abb0de39a4d
Albert Safin <xzfcpw@gmail.com>
2020-05-22 10:24:32 +0000

    tests: Simplify t02-file-dep.sh

--- tests/t02-file-dep.sh
+++ tests/t02-file-dep.sh
@@ -3,36 +3,21 @@
 # Check that cache in invalidated when indirecly used .nix file is updated.
 
 put ./tmp/small.nix << 'EOF'
-{ stdenv }:
-let str = import ./foo.nix;
-in stdenv.mkDerivation {
-  name = "cached-nix-shell-test";
-  unpackPhase = ":";
-  installPhase = ''
-    mkdir -p $out/bin
-    echo 'echo ${str}' > $out/bin/x
-    chmod +x $out/bin/x
-  '';
-}
-EOF
-
-put +x ./tmp/run << 'EOF'
-#!/usr/bin/env cached-nix-shell
-#! nix-shell -i sh -p "callPackage ./small.nix {}"
-x
+with import <nixpkgs> { };
+mkShell { VAR = import ./foo.nix; }
 EOF
 
 
 echo '"val1"' > ./tmp/foo.nix
-run ./tmp/run
-check_contains val1
+run cached-nix-shell ./tmp/small.nix --run 'echo $VAR'
+check_contains '^val1$'
 check_slow
 
-run ./tmp/run
-check_contains val1
+run cached-nix-shell ./tmp/small.nix --run 'echo $VAR'
+check_contains '^val1$'
 check_fast
 
 echo '"val2"' > ./tmp/foo.nix
-run ./tmp/run
-check_contains val2
+run cached-nix-shell ./tmp/small.nix --run 'echo $VAR'
+check_contains '^val2$'
 check_slow
