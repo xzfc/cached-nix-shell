a0a7ca18f1f3bb30a704ea01a6030ae107197c09
Albert Safin <xzfcpw@gmail.com>
2020-07-13 23:26:11 +0000

    Move postBuild and postInstall into nix/Makefile
    This should cause less fuss when bumping cached-nix-shell version in the
    downstream (aka Nixpkgs).

--- default.nix
+++ default.nix
@@ -13,22 +13,6 @@ in (naersk.buildPackage {
   else
     "next";
   BLAKE3_CSRC = "${blake3-src}/c";
-  postBuild = ''
-    ronn -r cached-nix-shell.1.md
-  '';
-  postInstall = ''
-    mkdir -p $out/lib
-    cp target/release/build/cached-nix-shell-*/out/trace-nix.so $out/lib/
-
-    mkdir -p $out/share/cached-nix-shell
-    cp rcfile.sh $out/share/cached-nix-shell/
-
-    mkdir -p $out/share/man/man1
-    cp cached-nix-shell.1 $out/share/man/man1/
-
-    mkdir -p $out/libexec/cached-nix-shell
-    ln -s $out/bin/cached-nix-shell $out/libexec/cached-nix-shell/nix-shell
-
-    mkdir -p $out/var/empty
-  '';
+  postBuild = "make -f nix/Makefile post-build";
+  postInstall = "make -f nix/Makefile post-install";
 })
