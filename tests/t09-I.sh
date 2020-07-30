27d1bf5658d666ab438e1cf4c75b253cf1ca4591
Albert Safin <xzfcpw@gmail.com>
2020-07-30 22:47:34 +0000

    Update dependencies
    * Update cargo and niv dependencies
    * Pin remove_dir_all
    * Switch to new Cargo.toml format
    * Update niv
    * Update pinned nixpkgs in a testcase

--- tests/t09-I.sh
+++ tests/t09-I.sh
@@ -5,7 +5,7 @@ run_inline << 'EOF'
 #! /usr/bin/env cached-nix-shell
 --[[
 #! nix-shell -i lua -p "luajit.withPackages (p: [ p.basexx ] )"
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz
 --]]
 
 print(require("basexx").to_base64("hello"))
