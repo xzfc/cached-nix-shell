59b11bf82c409529410f066d6c2159fac9aa9aa7
Albert Safin <xzfcpw@gmail.com>
2023-05-27 03:38:47 +0000

    shell.nix: Add rust tooling

--- shell.nix
+++ shell.nix
@@ -4,8 +4,11 @@ in with pkgs;
 mkShell {
   buildInputs = main.buildInputs ++ main.nativeBuildInputs ++ [
     cargo-edit
+    clippy
     niv
     nixfmt
+    rust-analyzer
+    rustc
     rustfmt
     shellcheck
     # Required for cargo
@@ -14,4 +17,5 @@ mkShell {
   ];
   inherit (main) BLAKE3_CSRC;
   CNS_IN_NIX_SHELL = "1";
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
