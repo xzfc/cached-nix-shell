f64b7508a531ee2635d17039dc4a79e46b582144
xzfc <xzfcpw@gmail.com>
2024-11-10 21:09:47 +0000

    style(*.nix): switch `nixfmt-classic` to `nixfmt-rfc-style`

--- default.nix
+++ default.nix
@@ -1,18 +1,24 @@
-{ pkgs ? import <nixpkgs> { }, nix ? pkgs.nix }:
+{
+  pkgs ? import <nixpkgs> { },
+  nix ? pkgs.nix,
+}:
 let
   sources = import ./nix/sources.nix;
   naersk = pkgs.callPackage sources.naersk { };
   gitignoreSource = (pkgs.callPackage sources.gitignore { }).gitignoreSource;
   blake3-src = sources.BLAKE3;
-in (naersk.buildPackage {
+in
+(naersk.buildPackage {
   root = gitignoreSource ./.;
-  buildInputs = [ pkgs.openssl nix pkgs.ronn ];
-}).overrideAttrs (attrs: {
-  CNS_GIT_COMMIT = if builtins.pathExists ./.git then
-    pkgs.lib.commitIdFromGitRepo ./.git
-  else
-    "next";
-  BLAKE3_CSRC = "${blake3-src}/c";
-  postBuild = "make -f nix/Makefile post-build";
-  postInstall = "make -f nix/Makefile post-install";
-})
+  buildInputs = [
+    pkgs.openssl
+    nix
+    pkgs.ronn
+  ];
+}).overrideAttrs
+  (attrs: {
+    CNS_GIT_COMMIT = if builtins.pathExists ./.git then pkgs.lib.commitIdFromGitRepo ./.git else "next";
+    BLAKE3_CSRC = "${blake3-src}/c";
+    postBuild = "make -f nix/Makefile post-build";
+    postInstall = "make -f nix/Makefile post-install";
+  })
--- shell.nix
+++ shell.nix
@@ -1,20 +1,27 @@
-{ pkgs ? import <nixpkgs> { } }:
-let main = import ./default.nix { inherit pkgs; };
-in with pkgs;
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  main = import ./default.nix { inherit pkgs; };
+in
+with pkgs;
 mkShell {
-  buildInputs = main.buildInputs ++ main.nativeBuildInputs ++ [
-    cargo-edit
-    clippy
-    niv
-    nixfmt
-    rust-analyzer
-    rustc
-    rustfmt
-    shellcheck
-    # Required for cargo
-    git
-    openssh
-  ];
+  buildInputs =
+    main.buildInputs
+    ++ main.nativeBuildInputs
+    ++ [
+      cargo-edit
+      clippy
+      niv
+      nixfmt-rfc-style
+      rust-analyzer
+      rustc
+      rustfmt
+      shellcheck
+      # Required for cargo
+      git
+      openssh
+    ];
   inherit (main) BLAKE3_CSRC;
   CNS_IN_NIX_SHELL = "1";
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
