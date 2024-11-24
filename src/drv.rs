c9cd8abe1a2b6251370f4faa2ee40a8056f80037
xzfc <xzfcpw@gmail.com>
2024-11-24 16:04:52 +0000

    perf: faster drv parser

--- src/drv.rs
+++ src/drv.rs
@@ -1,140 +1,155 @@
-use nom::branch::alt;
-use nom::bytes::complete::{escaped_transform, is_not, tag};
-use nom::character::complete::char;
-use nom::combinator::{eof, opt, value};
-use nom::multi::separated_list0;
-use nom::sequence::{delimited, preceded, tuple};
-use nom::{AsChar, IResult, InputIter, Slice};
-use std::ffi::{OsStr, OsString};
-use std::fmt::Debug;
+use std::collections::HashSet;
+use std::ffi::OsStr;
 use std::fs::read;
-use std::ops::RangeFrom;
-use std::os::unix::prelude::OsStringExt;
+use std::os::unix::ffi::OsStrExt as _;
 use std::path::Path;
-
-#[derive(Debug)]
-#[allow(dead_code)]
-struct Derivation {
-    outputs: Vec<(OsString, OsString, OsString, OsString)>,
-    input_drvs: Vec<(OsString, Vec<OsString>)>,
-    input_srcs: Vec<OsString>,
-    platform: OsString,
-    builder: OsString,
-    args: Vec<OsString>,
-    env: Vec<(OsString, OsString)>,
-}
 
 /// Check if .drv file is present, and all of its inputs (both .drv and their
 /// outputs) are present.
 pub fn derivation_is_ok<P: AsRef<OsStr>>(path: P) -> Result<(), String> {
     // nix-shell doesn't create an output for the shell derivation, so we
     // check it's dependencies instead.
-    for (drv, outputs) in load_derive(&path)?.input_drvs {
-        let parsed_drv = load_derive(&drv)?;
-        for out_name in outputs {
-            let name = &parsed_drv
-                .outputs
-                .iter()
-                .find(|(name, _, _, _)| name == &out_name)
-                .ok_or_else(|| {
-                    format!(
-                        "{}: output {:?} not found",
-                        drv.to_string_lossy(),
-                        out_name
-                    )
-                })?
-                .1;
-            if !Path::new(&name).exists() {
-                return Err(format!("{}: not found", name.to_string_lossy()));
-            }
+
+    let mut files_to_check = HashSet::new();
+
+    parse_derive(
+        Path::new(path.as_ref()),
+        |_, _| (),
+        |drv_path, outputs| {
+            parse_derive(
+                Path::new(OsStr::from_bytes(drv_path)),
+                |name, path| {
+                    if outputs.contains(&name) {
+                        files_to_check.insert(path.to_vec());
+                    }
+                },
+                |_, _| Ok(()),
+            )
+        },
+    )?;
+
+    for fname in files_to_check.into_iter() {
+        let path = Path::new(OsStr::from_bytes(&fname));
+        if !path.exists() {
+            return Err(format!("{}: not found", path.display()));
         }
     }
+
     Ok(())
 }
 
-fn load_derive<P: AsRef<OsStr>>(path: P) -> Result<Derivation, String> {
-    let data = read(path.as_ref())
-        .map_err(|e| format!("{}: !{}", path.as_ref().to_string_lossy(), e))?;
-    parse_derive(&data).map(|a| a.1).map_err(|_| {
-        format!("{}: failed to parse", path.as_ref().to_string_lossy())
-    })
+/// Load .drv file and call handlers on parsed data.
+/// For the reference, the .drv file format is:
+/// ```text
+/// Derive(
+///     // Outputs, passed to `handle_output`.
+///     // E.g. `("out", "/nix/store/…-stdenv-linux", "", "")`.
+///     [(name, path, hash_algo, hash), …],
+///
+///     // Inputs, passed to `handle_input`.
+///     // E.g. `("/nix/store/…-glibc-2.40-36.drv", ["bin", "dev", "out"])`.
+///     [(path, [name, …]), …],
+///
+///     // The rest is ignored by this function.
+///     [input_src, …],
+///     platform,         // Platform, e.g. "x86_64-linux".
+///     builder,          // Builder path, e.g. "/nix/store/…/bin/bash".
+///     [args, …],        // Builder arguments.
+///     [(key, value), …] // Environment.
+/// )
+/// ```
+fn parse_derive<T>(
+    path: &Path,
+    mut handle_output: impl FnMut(&[u8], &[u8]),
+    mut handle_input: impl FnMut(&[u8], &[&[u8]]) -> Result<T, String>,
+) -> Result<(), String> {
+    let input =
+        read(path).map_err(|e| format!("{}: !{}", path.display(), e))?;
+    let input = input.as_slice();
+
+    let input = tag(input, "Derive(")?;
+    let input =
+        parse_list(input, |input| parse_output(input, &mut handle_output))?;
+    let input = tag(input, ",")?;
+    let input =
+        parse_list(input, |input| handle_input_drv(input, &mut handle_input))?;
+    let input = tag(input, ",")?;
+    let _ = input; // skip rest
+    Ok(())
 }
 
-fn parse_derive(input: &[u8]) -> IResult<&[u8], Derivation> {
-    let (input, values) = delimited(
-        tag("Derive"),
-        tuple((
-            preceded(char('('), parse_list(parse_output)),
-            preceded(char(','), parse_list(parse_input_drv)),
-            preceded(char(','), parse_list(parse_string)),
-            preceded(char(','), parse_string),
-            preceded(char(','), parse_string),
-            preceded(char(','), parse_list(parse_string)),
-            preceded(char(','), parse_list(parse_env)),
-        )),
-        preceded(char(')'), eof),
-    )(input)?;
+fn parse_output<'a>(
+    input: &'a [u8],
+    mut handle_output: impl FnMut(&'a [u8], &'a [u8]),
+) -> Result<&'a [u8], String> {
+    let input = tag(input, "(")?;
+    let (input, name) = parse_string(input)?;
+    let input = tag(input, ",")?;
+    let (input, path) = parse_string(input)?;
+    let input = tag(input, ",")?;
+    let (input, _) = parse_string(input)?;
+    let input = tag(input, ",")?;
+    let (input, _) = parse_string(input)?;
+    let input = tag(input, ")")?;
 
-    let result = Derivation {
-        outputs: values.0,
-        input_drvs: values.1,
-        input_srcs: values.2,
-        platform: values.3,
-        builder: values.4,
-        args: values.5,
-        env: values.6,
-    };
-    Ok((input, result))
+    handle_output(name, path);
+    Ok(input)
 }
 
-fn parse_output(
-    input: &[u8],
-) -> IResult<&[u8], (OsString, OsString, OsString, OsString)> {
-    tuple((
-        preceded(char('('), parse_string),
-        preceded(char(','), parse_string),
-        preceded(char(','), parse_string),
-        delimited(char(','), parse_string, char(')')),
-    ))(input)
+fn handle_input_drv<'a, T>(
+    input: &'a [u8],
+    mut handle_input: impl FnMut(&'a [u8], &[&'a [u8]]) -> Result<T, String>,
+) -> Result<&[u8], String> {
+    let input = tag(input, "(")?;
+    let (input, drv_path) = parse_string(input)?;
+    let input = tag(input, ",")?;
+    let mut outputs = Vec::new();
+    let input = parse_list(input, |input| {
+        let (input, output) = parse_string(input)?;
+        outputs.push(output);
+        Ok(input)
+    })?;
+    let input = tag(input, ")")?;
+    handle_input(drv_path, &outputs)?;
+    Ok(input)
 }
 
-fn parse_input_drv(input: &[u8]) -> IResult<&[u8], (OsString, Vec<OsString>)> {
-    tuple((
-        preceded(char('('), parse_string),
-        delimited(char(','), parse_list(parse_string), char(')')),
-    ))(input)
+fn tag<'a>(input: &'a [u8], tag: &str) -> Result<&'a [u8], String> {
+    if input.starts_with(tag.as_bytes()) {
+        Ok(&input[tag.len()..])
+    } else {
+        Err(format!("parse error: expected {tag:?}"))
+    }
 }
 
-fn parse_env(input: &[u8]) -> IResult<&[u8], (OsString, OsString)> {
-    tuple((
-        preceded(char('('), parse_string),
-        delimited(char(','), parse_string, char(')')),
-    ))(input)
+fn parse_list<'a>(
+    mut input: &'a [u8],
+    mut f: impl FnMut(&'a [u8]) -> Result<&'a [u8], String>,
+) -> Result<&'a [u8], String> {
+    input = tag(input, "[")?;
+    loop {
+        input = f(input)?;
+        match input.first() {
+            Some(b',') => input = &input[1..],
+            Some(b']') => return Ok(&input[1..]),
+            _ => return Err("parse error: expected , or ]".to_string()),
+        }
+    }
 }
 
-fn parse_list<I, O, E, F>(f: F) -> impl FnMut(I) -> IResult<I, Vec<O>, E>
-where
-    I: Clone + nom::InputLength + Slice<RangeFrom<usize>> + InputIter,
-    <I as InputIter>::Item: AsChar,
-    F: nom::Parser<I, O, E>,
-    E: nom::error::ParseError<I>,
-{
-    delimited(char('['), separated_list0(char(','), f), char(']'))
-}
-
-fn parse_string(input: &[u8]) -> IResult<&[u8], OsString> {
-    let (input, parsed) = delimited(
-        char('\"'),
-        opt(escaped_transform(
-            is_not("\\\""),
-            '\\',
-            alt((
-                value(b"\\" as &'static [u8], char('\\')),
-                value(b"\"" as &'static [u8], char('"')),
-                value(b"\n" as &'static [u8], char('n')),
-            )),
-        )),
-        char('\"'),
-    )(input)?;
-    Ok((input, OsString::from_vec(parsed.unwrap_or_default())))
+fn parse_string(input: &[u8]) -> Result<(&[u8], &[u8]), String> {
+    let input = tag(input, "\"")?;
+    let mut backslash = false;
+    for (n, &c) in input.iter().enumerate() {
+        if backslash {
+            backslash = false;
+        } else {
+            match c {
+                b'\\' => backslash = true,
+                b'"' => return Ok((&input[n + 1..], &input[..n])),
+                _ => (),
+            }
+        }
+    }
+    Err("parse error: expected \"".to_string())
 }
